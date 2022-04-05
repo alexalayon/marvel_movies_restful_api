af5bc0b267a5aa9dcf53d0e6cf94979c08680373
samanalyst <102742210+samanalyst@users.noreply.github.com>
2022-04-05 21:49:57 +0100

    added endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, json, request
-from db import create_record
+from db import create_record, delete_record
 from classes.characters import Character
 from classes.movies import Movie
 
@@ -30,6 +30,37 @@ def create_character():
         return jsonify({'error': 'there is an error with: ' + str(ex)}), 400
 
     return jsonify({'message':result}), 201
+
+
+@app.route("/movies/", methods=['DELETE'])
+def delete_movie():
+    try:
+        
+            
+
+        movietodelete = request.args.get('movietodelete')
+        result = delete_record(movietodelete, "movies")
+        
+        
+    except Exception as ex:
+        return jsonify({'error': 'there is an error with :' + str(ex)}), 400
+
+    return jsonify({'message': result}), 201
+    
+@app.route("/characters/", methods=['DELETE'])
+def delete_character():
+    try:
+        
+            
+
+        charactertodelete = request.args.get('charactertodelete')
+        result = delete_record(charactertodelete, "characters")
+        
+        
+    except Exception as ex:
+        return jsonify({'error': 'there is an error with :' + str(ex)}), 400
+
+    return jsonify({'message': result}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
